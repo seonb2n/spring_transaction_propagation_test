e018c3adfb1c46a516c10508666aa85d1429a7c3
seonb2n <626ksb@gmail.com>
2023-11-12 13:44:37 +0900

    - init project

---
+++ src/main/java/com/example/propagation_test/PropagationTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.propagation_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropagationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropagationTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/propagation_test/PropagationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.propagation_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropagationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
